1ac3b7416f1aca98c8b30adffa1b5e52ce9cd965
lzw <lzw@xzy.com>
2021-08-27 12:12:21 +0800

    push commit

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -3,5 +3,6 @@ public class GitTest {
         System.out.println("hello git!");
         System.out.println("hello lzw");
         System.out.println("hello");
+        System.out.println("gittest");
     }
 }
