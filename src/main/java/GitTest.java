0731592734edc68656cea7547254452954486728
lzwhnjb <76190698+lzwhnjb@users.noreply.github.com>
2021-08-27 15:15:28 +0800

    gitee test

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("hello");
         System.out.println("gittest");
         System.out.println("pull-test");
+        System.out.println("gitee-test");
     }
 }
